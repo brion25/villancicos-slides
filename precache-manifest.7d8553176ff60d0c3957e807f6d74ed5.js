c52a63f4d21e41f68fe8ac7456a158b996fac5f4
JC Gomez <car_25_ixco@hotmail.com>
2019-12-17 13:03:46 -0600

    Updates

---
+++ precache-manifest.7d8553176ff60d0c3957e807f6d74ed5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1379d187575369d10f9f5ba1020055",
+    "url": "/villancicos-slides/index.html"
+  },
+  {
+    "revision": "e5bedef1d96abcd50c0e",
+    "url": "/villancicos-slides/static/css/2.d5831cfa.chunk.css"
+  },
+  {
+    "revision": "321a9f973ff09acecef8",
+    "url": "/villancicos-slides/static/css/main.b2231523.chunk.css"
+  },
+  {
+    "revision": "e5bedef1d96abcd50c0e",
+    "url": "/villancicos-slides/static/js/2.f808af9d.chunk.js"
+  },
+  {
+    "revision": "321a9f973ff09acecef8",
+    "url": "/villancicos-slides/static/js/main.f0649961.chunk.js"
+  },
+  {
+    "revision": "bc0e3e388ccbd6b397b7",
+    "url": "/villancicos-slides/static/js/runtime~main.f55c2589.js"
+  }
+]);
